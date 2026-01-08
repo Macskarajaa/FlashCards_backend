92e94c54ca5ec8e06e92998e03e131e85218cad1
unknown <ftamas2006@gmail.com>
2026-01-08 10:22:46 +0100

    before final

--- index.js
+++ index.js
@@ -1,52 +1,51 @@
-import express from 'express';
-import jwt from "jsonwebtoken";
-import cors from 'cors';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+import express from "express"
+import jwt from "jsonwebtoken"
+import cors from "cors"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
-app.use(cors({ 
+app.use(cors({
     origin:process.env.FRONTEND_URL,
     credentials:true
-}));
+}))
 
-app.use(express.json());
-app.use(cookieParser());
+app.use(express.json())
+app.use(cookieParser())
 
-app.post('/login', (req, resp) => {
+app.post("/login",(req,resp)=>{
     const {key} = req.body;
-    if(key !== process.env.AUTH_KEY) return resp.status(401).json({message: 'Wrong key'});
-    const token = jwt.sign({access: true}, process.env.JWT_SECRET, {expiresIn: '2h'});
-    resp.cookie("token", token, {
-        httpOnly: true,
-        secure:false,
-        sameSite: 'strict',
+    if(key!==process.env.AUTH_KEY) return resp.status(401).json({error:"Hibás kulcs!"})
+    const token = jwt.sign({access:true},process.env.JWT_SECRET,{expiresIn:"2h"})
+    const isProd = process.env.NODE_ENV ==="production"
+    resp.cookie("token",token,{
+        httpOnly:true,
+        secure:isProd,
+        sameSite:isProd? "none" : "strict",
         maxAge:2*60*60*1000,
     })
-    resp.sendStatus(200);
+    resp.sendStatus(200)
 })
 
-app.get("/protected", (req, resp) => {
-    try{
-    const token = req.cookies.token;
-    if(!token) return resp.status(401).json({message: 'No token provided'});
-    jwt.verify(token, process.env.JWT_SECRET)
-    resp.sendStatus(200);
+app.get("/protected",(req,resp)=>{
+    try {
+        const token = req.cookies.token
+        if(!token) throw new Error();
+        jwt.verify(token,process.env.JWT_SECRET)
+        resp.sendStatus(200)
     } catch (error) {
-        return resp.status(401).json({message: 'Invalid token'});
+        resp.sendStatus(401)
     }
 })
 
-app.post('/logout', (req, resp) => {
-    resp.clearCookie("token");
-    resp.sendStatus(200);
-});
+app.post("/logout",(req,resp)=>{
+    resp.clearCookie("token")
+    resp.sendStatus(200)
+})
 
 
 const port = 3000
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
- 
+app.listen(port,()=>console.log(`server listening on port: ${port}`))
